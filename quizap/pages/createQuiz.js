2a996618c7b745f6c0b4104b5de93c8d0800b204
Amber <ahmborr@gmail.com>
2024-05-02 19:52:07 -0700

    createQuiz done

--- quizap/pages/createQuiz.js
+++ quizap/pages/createQuiz.js
@@ -1,7 +1,26 @@
 import styles from "@/styles/createQuiz.module.css"
+import Image from "next/image";
+import { useState } from 'react';
 
 const creeateQuiz = () => {
+    const [imageUrl, setImageUrl] = useState('/noImage.png');
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        setImageUrl(URL.createObjectURL(file));
+      };
 
+    return (  <main className={styles.main}>
+        <div className={styles.body}>
+            <h1 className={styles.h1}>Make a New Quiz!</h1>
+            <input className={styles.input} type="text" id ="first" name="first" placeholder="NAME" required></input>
+
+            <div className={styles.style}><Image src={imageUrl} id= "pic" width={170} height={170}/>
+                <label className={styles.label} for="input-file">Edit Image</label>
+                <input className={styles.imageInput} type="file" accept="image/jepg, image/png, image/jpg" id="input-file" onChange={handleImageChange}></input>
+            </div>
+            <div class="wrap"><button className={styles.button} type="submit" onclick="solve()">Create Quiz</button></div>
+        </div>
+    </main> );
 }
 
 export default creeateQuiz;
