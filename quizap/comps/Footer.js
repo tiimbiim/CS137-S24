9c1627f7c60e3a46e8fb825fc6853c9a9523a6df
Quamin Hua <quaminhua56@gmail.com>
2024-05-03 19:19:24 -0700

    new footer and index page css

--- quizap/comps/Footer.js
+++ quizap/comps/Footer.js
@@ -1,39 +1,27 @@
-import styles from "@/styles/Home.module.css";
+import styles from "@/styles/footer.module.css";
 import Link from "next/link";
 
-const sectionStyle = {
-    display: 'inline-block',
-    verticalAlign: 'top',
-    textAlign: 'center',
-    marginBottom: '20px',
-};
-const listStyle = {
-    listStyle: 'none',
-    textAlign: 'left',
-}
-const textStyle = {
-    marginBottom: '15px',
-}
-
 const Footer = () => {
-    return ( 
+    return (
         <footer className={styles.footerStyle}>
-            <div style={sectionStyle}>
-                <h2>About</h2>
-                <ul style={listStyle}>
-                    <li style={textStyle}><Link href="/info/howItWorks">How QuiZap works</Link></li>
-                    <li style={textStyle}><Link href="/info/aboutUs">Meet the team</Link></li>
-                    <li style={textStyle}><Link href="/info/contactUs">Contact us</Link></li>
-                </ul>
-            </div>
-            <div style={sectionStyle}>
-                <h2>Help Center</h2>
-                <ul style={listStyle}>
-                    <li style={textStyle}><Link href="/info/FAQ">FAQ</Link></li>
-                    <li style={textStyle}><Link href="/info/communityForums">Community Forums</Link></li>
-                    <li style={textStyle}><Link href="/info/reportIssues">Report Issues</Link></li>
-                    <li style={textStyle}><Link href="/info/legalAndPrivacy">Legal & Privacy</Link></li>
-                </ul>
+            <div className={styles.footerLinksSection}>
+                <div className={styles.footerColumn}>
+                    <h2>About</h2>
+                    <ul className={styles.listStyle}>
+                        <li className={styles.textStyle}><Link href="/info/howItWorks">How QuiZap works</Link></li>
+                        <li className={styles.textStyle}><Link href="/info/aboutUs">Meet the team</Link></li>
+                        <li className={styles.textStyle}><Link href="/info/contactUs">Contact us</Link></li>
+                    </ul>
+                </div>
+                <div className={styles.footerColumn}>
+                    <h2>Help Center</h2>
+                    <ul className={styles.listStyle}>
+                        <li className={styles.textStyle}><Link href="/info/FAQ">FAQ</Link></li>
+                        <li className={styles.textStyle}><Link href="/info/communityForums">Community Forums</Link></li>
+                        <li className={styles.textStyle}><Link href="/info/reportIssues">Report Issues</Link></li>
+                        <li className={styles.textStyle}><Link href="/info/legalAndPrivacy">Legal & Privacy</Link></li>
+                    </ul>
+                </div>
             </div>
             <p>Copyright © 2024 QuiZap</p>
         </footer>
--- quizap/comps/ImageComp.js
+++ quizap/comps/ImageComp.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import styles from "@/styles/imageComp.module.css";
 
 const containerStyle = {
     alignItems: 'center',
@@ -9,8 +10,7 @@ const containerStyle = {
 const imageStyle = {
     marginTop: '-80px',
     width: '100vw',
-    height: '750px',  
-
+    height: '750px',
 };
 
 const textStyle = {
@@ -32,11 +32,11 @@ const paragraphStyle = {
 
 const ImageComp = () => {
     return ( 
-        <div style={containerStyle}>
-            <Image src="/1125678.png" width={1280} height={1080} style={imageStyle}/>
-            <div style={textStyle}>
-                <h1>THE DAWN OF A NEW WAY TO STUDY</h1>
-                <p style={paragraphStyle}>QuiZap is the newest innovative studying platform, where learning meets engagement. 
+        <div className={styles.imageCompStyle}>
+            <Image src="/1125678.png" width={1280} height={1080} className={styles.imageStyle} alt="Background Image"/>
+            <div className={styles.contentStyle}>
+                <h1 className={styles.headingStyle}>THE DAWN OF A NEW WAY TO STUDY</h1>
+                <p className={styles.paragraphStyle}>QuiZap is the newest innovative studying platform, where learning meets engagement. 
                 With our user-friendly interface and diverse study tools, learners can tailor their educational journey, 
                 whether it’s through lively quiz competitions or focused flashcard review sessions. Join us in revolutionizing 
                 the way students study and succeed!</p>
