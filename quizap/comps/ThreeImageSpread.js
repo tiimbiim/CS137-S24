2643206813722b98117f5611ac521514a1331254
Quamin Hua <quaminhua56@gmail.com>
2024-05-03 19:35:38 -0700

    new three image spread stylesheet

--- quizap/comps/ThreeImageSpread.js
+++ quizap/comps/ThreeImageSpread.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import styles from "@/styles/threeImageSpread.module.css";
 
 const imageStyle = {
     width: '33.3%',
@@ -37,26 +38,26 @@ const paragraphStyle = {
 
 const ThreeImageSpread = () => {
     return ( 
-        <div className="row" style={rowStyle}>
-            <div className="column" style={imageStyle}>
+        <div className={styles.rowStyle}>
+            <div className={styles.columnStyle}>
                 <Image src="/SUB12E.png" width={500} height={400}/>
-                <div style={textStyle}>
-                    <h1 style={headerStyle}>Create Quizzes</h1>
-                    <p style={paragraphStyle}>Make quizzes with your own custom questions in just a couple clicks</p>
+                <div className={styles.textStyle}>
+                    <h1 className={styles.headerStyle}>Create Quizzes</h1>
+                    <p className={styles.paragraphStyle}>Make quizzes with your own custom questions in just a couple clicks</p>
                 </div>
             </div>
-            <div className="column" style={imageStyle}>
+            <div className={styles.columnStyle}>
                 <Image src="/SUB25E.png" width={500} height={400}/>
-                <div style={textStyle}>
-                    <h1 style={headerStyle}>Invite Friends</h1>
-                    <p style={paragraphStyle}>Invite friends via a link to join your quiz</p>
+                <div className={styles.textStyle}>
+                    <h1 className={styles.headerStyle}>Invite Friends</h1>
+                    <p className={styles.paragraphStyle}>Invite friends via a link to join your quiz</p>
                 </div>
             </div>
-            <div className="column" style={imageStyle}>
+            <div className={styles.columnStyle}>
                 <Image src="/SUB26E.png" width={500} height={400}/>
-                <div style={textStyle}>
-                    <h1 style={headerStyle}>Share Results</h1>
-                    <p style={paragraphStyle}>Share your quiz results and show the rest of the world you're the smartest</p>
+                <div className={styles.textStyle}>
+                    <h1 className={styles.headerStyle}>Share Results</h1>
+                    <p className={styles.paragraphStyle}>Share your quiz results and show the rest of the world you're the smartest</p>
                 </div>
             </div>
         </div>
