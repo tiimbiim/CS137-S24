2cbc1f5ac620c89a86562f4ce358b07051f4d592
Quamin Hua <quaminhua56@gmail.com>
2024-05-13 20:19:39 -0700

    New (temp?) assets for splash page

--- quizap/comps/ThreeImageSpread.js
+++ quizap/comps/ThreeImageSpread.js
@@ -6,21 +6,21 @@ const ThreeImageSpread = () => {
     return ( 
         <div className={styles.rowStyle}>
             <div className={styles.columnStyle}>
-                <Image src="/SUB12E.png" width={500} height={400}/>
+                <Image src="/Asset1.png" width={400} height={400}/>
                 <div className={styles.textStyle}>
                     <h1 className={styles.headerStyle}>Create Quizzes</h1>
                     <p className={styles.paragraphStyle}>Make quizzes with your own custom questions in just a couple clicks</p>
                 </div>
             </div>
             <div className={styles.columnStyle}>
-                <Image src="/SUB25E.png" width={500} height={400}/>
+                <Image src="/Asset2.png" width={400} height={400}/>
                 <div className={styles.textStyle}>
                     <h1 className={styles.headerStyle}>Invite Friends</h1>
                     <p className={styles.paragraphStyle}>Invite friends via a link to join your quiz</p>
                 </div>
             </div>
             <div className={styles.columnStyle}>
-                <Image src="/SUB26E.png" width={500} height={400}/>
+                <Image src="/Asset3.png" width={400} height={400}/>
                 <div className={styles.textStyle}>
                     <h1 className={styles.headerStyle}>Share Results</h1>
                     <p className={styles.paragraphStyle}>Share your quiz results and show the rest of the world you're the smartest</p>
