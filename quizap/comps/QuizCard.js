e010d5d3903947437fbd842b24c215de54cee338
Quamin Hua <quaminhua56@gmail.com>
2024-05-16 17:18:47 -0700

    Quiz Card now links to quiz page

--- quizap/comps/QuizCard.js
+++ quizap/comps/QuizCard.js
@@ -1,12 +1,13 @@
 import styles from "@/styles/quizCard.module.css";
+import Link from "next/link";
 
 const QuizCard = ({ quizName, numQuestions, quizOwner}) => {
     return (
-        <div className={styles.quizCard}>
+        <Link href="quiz/ID" className={styles.quizCard}>
             <h1 className={styles.quizName}>{quizName}</h1>
             <h2 className={styles.numQuestions}>{numQuestions} Questions</h2>
             <p className={styles.quizOwner}>Created by: {quizOwner}</p>
-        </div>
+        </Link>
     )
 }
 
