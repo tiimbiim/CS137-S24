ff8e14721fb2e220642bfd4d6a3c1c7acf6ab10e
Quamin Hua <quaminhua56@gmail.com>
2024-05-17 21:00:32 -0700

    Commented out unnecessary links in sidebar

--- quizap/comps/SideBar.js
+++ quizap/comps/SideBar.js
@@ -4,8 +4,8 @@ const SideBar = () => {
             <a href="mainPage">Home</a>
             <a href="library">Library</a>
             <a href="discover">Discover</a>
-            <a href="stats">Stats</a>
-            <a href="schedule">Schedule</a>
+            {/* <a href="stats">Stats</a>
+            <a href="schedule">Schedule</a> */}
         </div>
      );
 }
