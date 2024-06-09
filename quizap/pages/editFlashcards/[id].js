441ee60ad150bd2995486566ac83fb7c6fdd015b
Quamin Hua <quaminhua56@gmail.com>
2024-06-08 21:55:31 -0700

    added backend functionality to flashcards

---
+++ quizap/pages/editFlashcards/[id].js
@@ -0,0 +1,110 @@
+import styles from "@/styles/createFlashcard.module.css";
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { doc, getDoc, updateDoc, arrayUnion, increment } from 'firebase/firestore';
+import { db, storage, auth } from '../../firebase.config'
+
+const CreateFlashcard = () => {
+    const [flashcards, setFlashcards] = useState([]);
+    const [idCounter, setIdCounter] = useState(1);
+    const [name, setName] = useState("");
+
+    const router = useRouter();
+    const flashcardsId = router.query.id;
+    const user = auth.currentUser;
+
+    useEffect(() => {
+        const fetchFlashcardsData = async () => {
+            if (flashcardsId) {
+                const flashcardsDocRef = doc(db, "flashcards", flashcardsId);
+                const flashcardsDoc = await getDoc(flashcardsDocRef);
+                if (flashcardsDoc.exists()) {
+                    const fetchedCards = flashcardsDoc.data().flashcards || [];
+                    let flashcardsArray = [];
+                    for (let i = 0; i < fetchedCards.length; i++) {
+                        flashcardsArray.push({ id: i, side1: fetchedCards[i].side1, side2: fetchedCards[i].side2 });
+                    }
+                    setFlashcards(flashcardsArray);
+                    setIdCounter(flashcardsArray.length + 1);
+                    setName(flashcardsDoc.data().name);
+                }
+            }
+        };
+        fetchFlashcardsData();
+    }, [flashcardsId]);
+
+    const handleAddFlashcard = () => {
+        setFlashcards([...flashcards, { id: idCounter, side1: '', side2: '' }]);
+        setIdCounter(idCounter + 1);
+    };
+    const handleFlashcardChange = (id, side, value) => {
+        const updatedFlashcards = flashcards.map(flashcard => {
+            if (flashcard.id === id) {
+                return { ...flashcard, [side]: value };
+            }
+            return flashcard;
+        });
+        setFlashcards(updatedFlashcards);
+    };
+    const handleDeleteFlashcard = (id) => {
+        const updatedFlashcards = flashcards.filter(flashcard => flashcard.id !== id);
+        setFlashcards(updatedFlashcards);
+    };
+
+    const handleSaveFlashcards = async () => {
+        if (name === '') {
+            alert('Please enter a name for your flashcards at the top.');
+            return;
+        }
+        const flashcardsDocRef = doc(db, "flashcards", flashcardsId);
+        try {
+            await updateDoc(flashcardsDocRef, {
+                name: name,
+                flashcards: flashcards
+            });
+            alert('Flashcards saved successfully!');
+            router.push('/library');
+        }
+        catch (error) {
+            console.error("Error saving flashcards:", error);
+        }
+    };
+    return (
+        <main className={styles.main}>
+            <div className={styles.body}>
+                <h1 className={styles.h1}>Flashcards</h1>
+                <h6 className={styles.h6}>ID: {flashcardsId}</h6>
+                <input className={styles.nameInput} type="text" placeholder="NAME" defaultValue={name} onChange={e => setName(e.target.value)} required />
+
+                {flashcards.map(flashcard => (
+                    <div key={flashcard.id} className={styles.card}>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            value={flashcard.side1}
+                            onChange={(e) => handleFlashcardChange(flashcard.id, 'side1', e.target.value)}
+                            placeholder={'SIDE 1'}
+                            required
+                        />
+                        <input
+                            className={styles.input}
+                            type="text"
+                            value={flashcard.side2}
+                            onChange={(e) => handleFlashcardChange(flashcard.id, 'side2', e.target.value)}
+                            placeholder={'SIDE 2'}
+                            required
+                        />
+                        <button className={styles.deleteButton} onClick={() => handleDeleteFlashcard(flashcard.id)}>X</button>
+                    </div>
+                ))}
+                <div className="wrap">
+                    <button className={styles.addButton} onClick={handleAddFlashcard}>+</button>
+                </div>
+                <div className="wrap"><button className={styles.saveButton} onClick={handleSaveFlashcards}>Save</button></div>
+                <a href="/library"><div><button>Back</button></div></a>
+            </div>
+        </main>
+    );
+}
+
+export default CreateFlashcard;
--- quizap/pages/library.js
+++ quizap/pages/library.js
@@ -3,14 +3,19 @@ import MainNavbar from "@/comps/MainNavbar";
 import Footer from "@/comps/Footer";
 import SideBar from "@/comps/SideBar";
 import React, { useState, useEffect } from 'react';
-import { doc, collection, getDocs } from 'firebase/firestore';
+import { doc, collection, getDocs, addDoc } from 'firebase/firestore';
 import { db, storage, auth } from '../firebase.config'
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const library = () => {
     const [activeTab, setActiveTab] = useState('quizzes');
     const [quizzes, setQuizzes] = useState([]);
+    const [flashcards, setFlashcards] = useState([]);
     const [users, setUsers] = useState({});
+
+    const router = useRouter();
+    const user = auth.currentUser;
 
     const handleTabClick = (tab) => {
         setActiveTab(tab);
@@ -30,9 +35,29 @@ const library = () => {
             setQuizzes(quizList);
             setUsers(userList);
         };
-    
+        const fetchFlashcards = async () => {
+            const querySnapshot = await getDocs(collection(db, "flashcards"));
+            const flashcardsList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            setFlashcards(flashcardsList);
+        }
         fetchQuizzes();
+        fetchFlashcards();
     }, []);
+
+    const handleAddFlashcards = async () => {
+        try {
+            const docRef = await addDoc(collection(db, "flashcards"), {
+                name: "",
+                flashcards: [],
+                owner: user.uid
+            });
+            router.push(`/editFlashcards/${docRef.id}`);
+        }
+        catch {
+            alert("There was an error creating your flashcards!");
+        }
+    }
+
     return ( 
         <div>
             <MainNavbar />
@@ -43,9 +68,14 @@ const library = () => {
                         <button onClick={() => handleTabClick('quizzes')} className={activeTab === 'quizzes' ? styles.active : styles.inactive}>Quizzes</button>
                         <button onClick={() => handleTabClick('flashcards')} className={activeTab === 'flashcards' ? styles.active : styles.inactive}>Flashcards</button>
                     </div>
-                    <a href={activeTab === 'quizzes' ? "createQuiz" : "createFlashcard"} className={styles.addButton}>
+                    {activeTab === 'quizzes' &&
+                    <a href={"createQuiz"} className={styles.addButton}>
                         +
-                    </a>
+                    </a>}
+                    {activeTab === 'flashcards' &&
+                    <button className={styles.addButton} onClick={handleAddFlashcards}>
+                        +
+                    </button>}
                     <div>
                         {activeTab === 'quizzes' && (
                             <ul>
@@ -61,12 +91,14 @@ const library = () => {
                         )}
                         {activeTab === 'flashcards' && (
                             <ul>
-                                <li className={styles.listStyle}><a className={styles.anchorStyle} href="createFlashcard">aaa | Created by Xxxxx | 1000 flashcards | Mar 10 2024</a><a className={styles.playButton} href="flashcards/ID">Play</a></li>
-                                <li className={styles.listStyle}><a className={styles.anchorStyle} href="createFlashcard">bbb | Created by Xxxxx | 50 flashcards | Feb 10 2024</a><a className={styles.playButton} href="flashcards/ID">Play</a></li>
-                                <li className={styles.listStyle}><a className={styles.anchorStyle} href="createFlashcard">ccc | Created by Xxxxx | 25 flashcards | Feb 6 2024</a><a className={styles.playButton} href="flashcards/ID">Play</a></li>
-                                <li className={styles.listStyle}><a className={styles.anchorStyle} href="createFlashcard">ddd | Created by Xxxxx | 35 flashcards | Jan 20 2024</a><a className={styles.playButton} href="flashcards/ID">Play</a></li>
-                                <li className={styles.listStyle}><a className={styles.anchorStyle} href="createFlashcard">eee | Created by Xxxxx | 50 flashcards | Jan 15 2024</a><a className={styles.playButton} href="flashcards/ID">Play</a></li>
-                                <li className={styles.listStyle}><a className={styles.anchorStyle} href="createFlashcard">fff | Created by Xxxxx | 60 flashcards | Jan 10 2024</a><a className={styles.playButton} href="flashcards/ID">Play</a></li>
+                                {flashcards.map((cards) => (
+                                    <li key={cards.id} className={styles.listStyle}>
+                                        <a className={styles.anchorStyle} href={`editFlashcards/${cards.id}`}>
+                                            {cards.name} | Created by {users[cards.owner]?.username} | {cards.flashcards.length} flashcards
+                                        </a>
+                                        <a className={styles.playButton} href={`flashcards/${cards.id}`}>Play</a>
+                                    </li>
+                                ))}
                             </ul>
                         )}
                     </div>
